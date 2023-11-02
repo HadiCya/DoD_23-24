ef03f6fa6434608869ef1e5e20d1e6c34960c9e9
HadiChaaban <chaabanh@umich.edu>
2023-11-02 10:45:47 -0400

    Removed Camera

--- Source/Gameplay/World/Map.cs
+++ Source/Gameplay/World/Map.cs
@@ -28,26 +28,22 @@ namespace DoD_23_24
 
         TiledMap _tiledMap;
         TiledMapRenderer _tiledMapRenderer;
-        Camera camera;
 
         public Map()
         {
             _tiledMap = Globals.content.Load<TiledMap>("LevelTiles/samplemap");
             _tiledMapRenderer = new TiledMapRenderer(Globals.graphics, _tiledMap);
             Globals.collision = _tiledMap.GetLayer<TiledMapTileLayer>("Collision");
-            camera = new Camera();
         }
 
         public virtual void Update(GameTime gameTime)
         {
             _tiledMapRenderer.Update(gameTime);
-
-            camera.Update(gameTime);
         }
 
         public virtual void Draw()
         {
-            _tiledMapRenderer.Draw(camera.getView());
+            _tiledMapRenderer.Draw();
         }
 
 
