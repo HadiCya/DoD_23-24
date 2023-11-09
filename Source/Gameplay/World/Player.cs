82a425b1c92a3d4ea300f94c19e51e327a04a175
HadiChaaban <chaabanh@umich.edu>
2023-11-09 14:30:28 -0500

    Added more abstraction

--- Source/Gameplay/World/Player.cs
+++ Source/Gameplay/World/Player.cs
@@ -21,11 +21,13 @@ namespace DoD_23_24
         Matrix translation;
         public Rectangle playerBounds;
         private float zoom = 2.0f;
+        private Level level;
 
-        public Player(string PATH, Vector2 POS, Vector2 DIMS, bool shouldScale) : base(PATH, POS, DIMS, shouldScale)
+        public Player(string PATH, Vector2 POS, Vector2 DIMS, bool shouldScale, Level level) : base(PATH, POS, DIMS, shouldScale)
 		{
             playerBounds = new Rectangle((int)pos.X - (int)(dims.X/2), (int)pos.Y - (int)(dims.Y / 2), (int)dims.X, (int)dims.Y);
-		}
+            this.level = level;
+        }
 
         public override void Update(GameTime gameTime)
         {
@@ -44,16 +46,8 @@ namespace DoD_23_24
         {
             var kstate = Keyboard.GetState();
 
-            if (kstate.IsKeyDown(Keys.Up))
-            {
-                pos.Y -= speed * zoom * (float)gameTime.ElapsedGameTime.TotalSeconds;
-            }
+            Vector2 initPos = pos;
 
-            if (kstate.IsKeyDown(Keys.Down))
-            {
-                pos.Y += speed * zoom * (float)gameTime.ElapsedGameTime.TotalSeconds;
-            }
-            
             if (kstate.IsKeyDown(Keys.Left))
             {
                 pos.X -= speed * zoom * (float)gameTime.ElapsedGameTime.TotalSeconds;
@@ -64,7 +58,19 @@ namespace DoD_23_24
                 pos.X += speed * zoom * (float)gameTime.ElapsedGameTime.TotalSeconds;
             }
             playerBounds.X = (int)pos.X - (int)(dims.X / 2);
+            if (level.CheckCollision(playerBounds)) pos.X = initPos.X;
+
+            if (kstate.IsKeyDown(Keys.Up))
+            {
+                pos.Y -= speed * zoom * (float)gameTime.ElapsedGameTime.TotalSeconds;
+            }
+
+            if (kstate.IsKeyDown(Keys.Down))
+            {
+                pos.Y += speed * zoom * (float)gameTime.ElapsedGameTime.TotalSeconds;
+            }
             playerBounds.Y = (int)pos.Y - (int)(dims.Y / 2);
+            if (level.CheckCollision(playerBounds)) pos.Y = initPos.Y;
         }
 
         private void CalculateTranslation()
@@ -72,6 +78,15 @@ namespace DoD_23_24
             var dx = (Globals.WIDTH / (zoom*2)) - playerBounds.X;
             var dy = (Globals.HEIGHT / (zoom*2)) - playerBounds.Y;
             translation = Matrix.CreateTranslation(dx, dy, 0f) * Matrix.CreateScale(zoom);
+        }
+
+        public Vector2 getPos() {
+            return pos;
+        }
+
+        public void setPos(Vector2 pos)
+        {
+            this.pos = pos;
         }
 
         public Matrix GetTranslation()
--- Source/Gameplay/World/World.cs
+++ Source/Gameplay/World/World.cs
@@ -16,52 +16,29 @@ using Microsoft.Xna.Framework.Media;
 using System.Reflection.Metadata;
 
 #endregion
-using TiledSharp;
 
 namespace DoD_23_24
 {
     public class World
     {
-        TileMapManager mapManager;
-        TmxMap map;
-        private List<Rectangle> collisionObjects;
 
+        Level level;
         Player playerInstance;
 
         public World()
         {
-            map = new TmxMap("Content/map.tmx");
-            Texture2D tileSet = Globals.content.Load<Texture2D>("Tiny Adventure Pack\\" + map.Tilesets[0].Name.ToString());
-            int tileWidth = map.Tilesets[0].TileWidth;
-            int tileHeight = map.Tilesets[0].TileHeight;
-            int tileSetTilesWide = tileSet.Width / tileWidth;
-            mapManager = new TileMapManager(Globals.spriteBatch, map, tileSet, tileSetTilesWide, tileWidth, tileHeight);
-
-            collisionObjects = new List<Rectangle>();
-            foreach(var o in map.ObjectGroups["Collisions"].Objects)
-            {
-                collisionObjects.Add(new Rectangle((int)o.X, (int)o.Y, (int)o.Width, (int)o.Height));
-            }
-
-            playerInstance = new Player("2D/Sprites/Item", new Vector2(100, 100), new Vector2(16, 16), true);
+            level = new Level("Content/map.tmx", "Tiny Adventure Pack\\");
+            playerInstance = new Player("2D/Sprites/Item", new Vector2(100, 100), new Vector2(16, 16), true, level);
         }
 
         public virtual void Update(GameTime gameTime)
         {
-            var initPos = playerInstance.pos;
             playerInstance.Update(gameTime);
-            foreach(var rect in collisionObjects)
-            {
-                if (rect.Intersects(playerInstance.playerBounds))
-                {
-                    playerInstance.pos = initPos;
-                }
-            }
         }
 
         public virtual void Draw()
         {
-            mapManager.Draw();
+            level.Draw();
             playerInstance.Draw();
         }
 
